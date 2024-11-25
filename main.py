e99497f335b99b7fce9d2e3190076fc5feccb78b
gna23 <gna23@sfu.ca>
2024-11-25 12:58:53 -0800

    init

--- main.py
+++ main.py
@@ -176,25 +176,25 @@ class PlaneDataset(Dataset):
 
   def numpy_to_tensor(self, img, mask):
     if self.tran is not None:
-
-        if random.random() > 0.5:
-            img = cv2.flip(img,1)
-            mask = cv2.flip(mask,1)
-        if random.random() > 0.5:
-            img = cv2.flip(img,0)
-            mask = cv2.flip(mask,0)
-        if random.random() > 0.3:
-            brightness = random.uniform(0.8, 1.2)
-            img = np.clip(img * brightness, 0, 255).astype(np.uint8)
-        if random.random() > 0.5:
-            angle = random.uniform(-30, 30)
-            h, w = img.shape[:2]
-            rotation_matrix = cv2.getRotationMatrix2D((w / 2, h / 2), angle, 1)
-            img = cv2.warpAffine(img, rotation_matrix, (w, h), flags=cv2.INTER_LINEAR)
-            mask = cv2.warpAffine(mask, rotation_matrix, (w, h), flags=cv2.INTER_NEAREST)
-
-        img = self.tran(img)
-        mask = self.tran(mask)
+        if self.set_name == "train":
+            if random.random() > 0.5:
+                img = cv2.flip(img,1)
+                mask = cv2.flip(mask,1)
+            if random.random() > 0.5:
+                img = cv2.flip(img,0)
+                mask = cv2.flip(mask,0)
+            if random.random() > 0.3:
+                brightness = random.uniform(0.8, 1.2)
+                img = np.clip(img * brightness, 0, 255).astype(np.uint8)
+            if random.random() > 0.5:
+                angle = random.uniform(-30, 30)
+                h, w = img.shape[:2]
+                rotation_matrix = cv2.getRotationMatrix2D((w / 2, h / 2), angle, 1)
+                img = cv2.warpAffine(img, rotation_matrix, (w, h), flags=cv2.INTER_LINEAR)
+                mask = cv2.warpAffine(mask, rotation_matrix, (w, h), flags=cv2.INTER_NEAREST)
+        else:
+            img = self.tran(img)
+            mask = self.tran(mask)
     img = torch.tensor(img, dtype=torch.float)
     mask = torch.tensor(mask, dtype=torch.float)
     return img, mask
