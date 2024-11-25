e2a1feb2ba6b10243cf2aab1f4c087aa11bdec48
gna23 <gna23@sfu.ca>
2024-11-24 17:41:54 -0800

    init

---
+++ requirements.sh
@@ -0,0 +1,7 @@
+pip install torchvision
+pip install opencv-python
+pip install scikit-learn
+pip install Pillow
+pip install pandas
+pip install pycocotools>=2.0.1
+python -m pip install --no-cache-dir 'git+https://github.com/facebookresearch/detectron2.git'
