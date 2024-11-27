abcc2e2ade995ce03c87b69d6163fc8c22ff7203
gna23 <gna23@sfu.ca>
2024-11-27 14:16:16 -0800

    init

--- main2.py
+++ main2.py
@@ -384,13 +384,8 @@ class PlaneDataset(Dataset):
 
         img, mask = self.numpy_to_tensor(img, mask)
         img = img.reshape((3,128,128))
-        if len(mask.shape)==1:
-            # import pdb
-            # pdb.set_trace()
-            mask = torch.zeros((1,128,128))
-            print("Error")
-        else:
-            mask = mask.reshape((1,128,128))
+
+        mask = mask.reshape((1, 128, 128))
 
         return img, mask
 
