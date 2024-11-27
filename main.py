3d236def8ce53fcad8a2ebf80beaf9c636cee7c4
gna23 <gna23@sfu.ca>
2024-11-26 17:31:36 -0800

    init

--- main.py
+++ main.py
@@ -192,23 +192,6 @@ def collate_fn(ech_data):
 
 
 
-def find_all_linear_names(model):
-    cls = torch.nn.Linear
-    lora_module_names = set()
-    multimodal_keywords = ['mm_projector', 'vision_tower', 'vision_resampler']
-    for name, module in model.named_modules():
-        if any(mm_keyword in name for mm_keyword in multimodal_keywords):
-            continue
-        if isinstance(module, cls):
-            names = name.split('.')
-            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
-
-    if 'lm_head' in lora_module_names: # needed for 16-bit
-        lora_module_names.remove('lm_head')
-    return list(lora_module_names)
-
-
-
 class CustomTrainer(DefaultTrainer):
     @classmethod
     def build_train_loader(cls, cfg):
@@ -221,13 +204,10 @@ class CustomTrainer(DefaultTrainer):
 
 # trainer = DefaultTrainer(cfg)
 
+cfg.MODEL.BACKBONE.FREEZE_AT = 4
+
+
 trainer = CustomTrainer(cfg)
-
-print("*********************")
-print(find_all_linear_names(trainer.model))
-0/0
-
-
 trainer.resume_or_load(resume=False)
 trainer.train()
 
