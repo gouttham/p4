c136f93ddd8befb2b6f0cab90b32188bc9a3eca1
gna23 <gna23@sfu.ca>
2024-12-08 00:26:44 -0800

    init

--- main.py
+++ main.py
@@ -118,10 +118,10 @@ def get_detection_data(set_name):
 
 
 TRAIN_DETECTION = False
-EVAL_DETECTION = True
+EVAL_DETECTION = False
 
-TRAIN_SEGMENTATION = False
-EVAL_SEGMENTATION = False
+TRAIN_SEGMENTATION = True
+EVAL_SEGMENTATION = True
 
 GEN_CSV = True
 RCNN_TRAIN = False
@@ -502,7 +502,7 @@ class MyModel(nn.Module):
 
 # Training
 # Set the hyperparameters
-num_epochs = 100
+num_epochs = 2
 batch_size = 4
 learning_rate = 1e-3
 weight_decay = 1e-5
